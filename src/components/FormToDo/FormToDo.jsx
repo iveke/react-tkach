2c0469555f4966ad4fa409672c3c484f0e43b4e9
Евеліна <evelina@MacBook-Pro-Andrey.local>
2024-02-22 21:51:02 +0200

    do something Form

--- src/components/FormToDo/FormToDo.jsx
+++ src/components/FormToDo/FormToDo.jsx
@@ -1,5 +1,17 @@
-
+import { Btn, Form, Input } from "./FormToDo.style";
 
 export const FormToDo = () => {
-    return <></>;
+    return (
+    <Form >
+        <Input name="title" type="text" placeholder="enter title task" />
+        <textarea name="description"cols="30" rows="4" placeholder="enter descriptiton"></textarea>
+        <select name="level"> 
+        <option value="1"> level 1</option>
+        <option value="2"> level 2</option>
+        <option value="3"> level 3 </option>
+        </select>
+        <Btn type="submit">Create</Btn>
+    </Form>
+    );
 }
+
---
+++ src/components/FormToDo/FormToDo.style.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  margin: 0 auto;
+  padding: 10px;
+  width: 300px;
+`;
+
+export const Input = styled.input`
+  margin-bottom: 10px;
+`;
+
+export const Btn = styled.button`
+  width:100px ;
+  height: 30px;
+  border: solid 2px black;
+  border-radius: 8px;
+  font-weight: bold;
+`;
