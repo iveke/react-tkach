a37f5e49b1b9de89139c73dfbb0f12b63bd2da75
Евеліна <evelina@MacBook-Pro-Andrey.local>
2024-04-26 11:11:31 +0300

    problem with click in another elem

--- src/components/Quation/QuationsCard.jsx
+++ src/components/Quation/QuationsCard.jsx
@@ -3,21 +3,29 @@ import { CardStyle } from "components/Quation/QuationsCard.style";
 import { useEffect, useState, useRef } from "react";
 import { CardsWrap } from "./QuationsCard.style";
 
-
+// let prevActive = null;
+// const initial = {
+//   item: "name",
+//   open: () => {},
+//   close: () => {},
+// };
 
 export function Cards() {
   const [active, setActive] = useState(null);
-  const [prevActive, setPrevActive] = useState(null);
-  useEffect(
-    () => {
-      console.log( active);
-    },
-    [active]
-  );
+  //   const [prevActive, setPrevActive] = useState(null);
+  //   useEffect(
+  //     () => {
+  //         console.log(prevActive, active)
+  //       if(active != prevActive){
+  //         console.log(prevActive)
+  //       }
+  //     },
+  //     [active]
+  //   );
 
   const handleAction = async (e, open, setOpen) => {
+    // if(active != e.currentTarget)
     const activeItem = e.currentTarget;
-    console.log(activeItem);
 
     if (open) {
       setActive(null);
