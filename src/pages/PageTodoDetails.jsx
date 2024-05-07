7ce98fbc581e8c72081b4b98c78e260f0daf8608
Евеліна <evelina@MacBook-Pro-Andrey.local>
2024-05-07 18:07:41 +0300

    do homework but have bug with useParams

--- src/pages/PageTodoDetails.jsx
+++ src/pages/PageTodoDetails.jsx
@@ -1,15 +1,17 @@
 import { useParams } from "react-router-dom";
 import { fetchToDoId } from "service/api";
 
-async function TodoDetails(){
-
-    const {id} = useParams();
-    console.log(id);
-    const res = await fetchToDoId(id);
-console.log(res)
-    return <div>
-        {/* // write something */}
+async function TodoDetails() {
+  const { id } = useParams();
+  const res = await fetchToDoId(id);
+  console.log(res);
+  return (
+    <div>
+      <h2>{res.title}</h2>
+      <p>{res.description}</p>
+      <p>{res.level}</p>
     </div>
+  );
 }
 
 export default TodoDetails;
