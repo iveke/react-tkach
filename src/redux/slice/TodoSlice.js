2508fe238ee62994c499392d46dfbe551c77b2cb
Евеліна <evelina@MacBook-Pro-Andrey.local>
2024-06-28 08:08:01 +0300

    add one fetchToDos and change in TodoSlice reducers to extrareducers, and adding thunk

--- src/redux/slice/TodoSlice.js
+++ src/redux/slice/TodoSlice.js
@@ -1,37 +1,18 @@
 import { createAction, createSlice } from "@reduxjs/toolkit";
 
-// export const todoReducer = (state = { list: [] }, action) => {
-//   switch (action.type) {
-//     case "todos/upload":
-//       return {
-//         ...state,
-//         list: action.payload,
-//       };
-//     case "todos/delete":
-//       return {
-//         ...state,
-//         todos: {
-//           ...state,
-//           list: state.todos.list.filter((item) => item.id !== action.payload),
-//         },
-//       };
-//     default:
-//       break;
-//   }
-//   return state;
-// };
-
-// export const changeList = createAction("todos/upload");
-// export const deleteItemList = createAction("todos/delete");
-
 
 const TodoSlice = createSlice({
     name: "todo",
-    initialState: {list:[]},
-    reducers: {
-        changeList(state, action){return action.payload},
-        deleteItemList(state,action){state.filter((item)=> item.id !== action.payload)}
-    }
+    initialState: {list:[],
+    isLoading: false, 
+error: null},
+    extraReducers: (builder) => builder.addCase(),
 })
 export const {changeList,deleteItemList} = TodoSlice.actions;
 export const todoReducer = TodoSlice.reducer;
+
+
+// reducers: {
+//     changeList(state, action){return action.payload},
+//     deleteItemList(state,action){state.filter((item)=> item.id !== action.payload)}
+// }
--- src/service/api.js
+++ src/service/api.js
@@ -1,3 +1,4 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 axios.defaults.baseURL = "https://65f01842da8c6584131ac041.mockapi.io/todoList";
@@ -23,3 +24,15 @@ export const deleteToDo = async (todoId) => {
   const res = await axios.delete(`/${todoId}`);
   return res.data;
 };
+
+export const fetchToDos =  createAsyncThunk(
+  "todo/fetchAll",
+  async(_, thunkAPI) => {
+    try {
+      const res = await axios.get("/");
+      return res.data;
+    } catch(error){
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+)
